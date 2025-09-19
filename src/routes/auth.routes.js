416c54ce69ebd3ed5ad6cb7a5a20fe555dfe49dd
rakhi <rakhisingh876.com@gmail.com>
2025-09-20 00:08:24 +0530

    Complete register API with JWT token

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const userModel = require('../model/user.model');
-
+const jwt= require("jsonwebtoken")
 const router = express.Router();
 
 router.post('/register',async(req,res)=>{
@@ -17,6 +17,14 @@ router.post('/register',async(req,res)=>{
     }
 
     const user= await userModel.create({username,password})
+
+    const token= jwt.sign({id: user._id},process.env.JWT_SECRET)
+
+    res.cookie("token",token)
+    res.status(201).json({
+        message:"user registered successfully",
+        user
+    })
 })
 
 
